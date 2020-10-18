2263c749a22217dc71186ef37c1892d6d9a2253e
yahye abdirahman <68783841+fikrado@users.noreply.github.com>
2020-10-18 23:55:56 +0300

    Update B0mb3r.py

--- B0mb3r.py
+++ B0mb3r.py
@@ -1,13 +1,13 @@
-from twilio.rest import Client
-
-account_sid = 'AC03705d2804ae2479f92c49220a5629ef'
-auth_token = 'a854428c0c2dd64fadca84dd67de73ab'
-client = Client(account_sid, auth_token)
-
-message = client.messages.create(
-    from_='whatsapp:+14155238886',
-    body=open("bee.txt")
-    to='whatsapp:+252633630649'
-)
-
+from twilio.rest import Client 
+ 
+account_sid = 'AC03705d2804ae2479f92c49220a5629ef' 
+auth_token = '[AuthToken]' 
+client = Client(account_sid, auth_token) 
+ 
+message = client.messages.create( 
+                              from_='whatsapp:+14155238886',  
+                              body=open("bee.txt",      
+                              to='whatsapp:+252634048063' 
+                          ) 
+ 
 print(message.sid)
